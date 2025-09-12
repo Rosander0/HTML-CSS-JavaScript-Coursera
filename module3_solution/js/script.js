eab17d712182f8b8837f37dd8b1fdaae7c75d555
Rosander0 <astereen.0107@gmail.com>
2025-09-12 13:42:19 +0530

    Done

---
+++ module3_solution/js/script.js
@@ -0,0 +1,120 @@
+(function (global) {
+
+// Set up a namespace for our utility
+var ajaxUtils = {};
+
+
+// Returns an HTTP request object
+function getRequestObject() {
+  if (window.XMLHttpRequest) {
+    return (new XMLHttpRequest());
+  } 
+  else {
+    global.alert("Ajax is not supported!");
+    return(null); 
+  }
+}
+
+
+// Makes an Ajax GET request to 'requestUrl'
+ajaxUtils.sendGetRequest = 
+  function(requestUrl, responseHandler, isJsonResponse) {
+    var request = getRequestObject();
+    request.onreadystatechange = 
+      function() { 
+        handleResponse(request, 
+                       responseHandler,
+                       isJsonResponse); 
+      };
+    request.open("GET", requestUrl, true);
+    request.send(null); // for POST only
+  };
+
+
+// Only calls user provided 'responseHandler'
+// function if response is ready
+// and not an error
+function handleResponse(request, responseHandler, isJsonResponse) {
+  if ((request.readyState == 4) &&
+     (request.status == 200)) {
+
+    // Default to isJsonResponse = true
+    if (isJsonResponse == undefined) {
+      isJsonResponse = true;
+    }
+
+    if (isJsonResponse) {
+      responseHandler(JSON.parse(request.responseText));
+    }
+    else {
+      responseHandler(request.responseText);
+    }
+  }
+}
+
+
+// Expose utility to the global object
+global.$ajaxUtils = ajaxUtils;
+
+})(window);
+
+
+
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+
+  // In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
+
+(function (global) {
+
+var dc = {};
+
+var homeHtml = "snippets/home-snippet.html";
+
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
+
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  var html = "<div class='text-center'>";
+  html += "<img src='images/ajax-loader.gif'></div>";
+  insertHtml(selector, html);
+};
+
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+
+// On first load, show home view
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+  homeHtml,
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
+  false);
+});
+
+
+global.$dc = dc;
+
+})(window);
